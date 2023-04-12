75034cc4d7899a40a90f9df37d2a3a239d5225fc
Bobby-jack <pigpongproduction@gmail.com>
2023-04-12 07:52:28 +0200

    styles allpage done

--- pages/allBookPage.js
+++ pages/allBookPage.js
@@ -19,34 +19,53 @@ export async function getStaticProps() {
 
 export default function AllBookPage({allJsonData}) {
     const [cat, setCat] = useState('')
+    const [minRat, setMinRat] = useState(0)
+    const [search, setSearch] = useState('')
     const connectedAccount = useSelector((state)=>state.account.connectedAccount)
     console.log(connectedAccount);
   return (
     <Layout>
         <div className={styles.main}>
             <div className={styles.filter}>
-                <input placeholder='Search by name'/>
-                <h3>Category</h3>
+                <input className={styles.input} value={search} onChange={(e)=>setSearch(e.target.value.toLowerCase())} placeholder='Search by name'/>
+                <h3>Category {cat}</h3>
                 <span onClick={()=>{setCat('')}}
                 className={cat==''?styles.category+' '+styles.selected:styles.category}>All</span>
-                <span onClick={()=>{setCat('')}}
+                <span onClick={()=>{setCat('classics')}}
                 className={cat=='classics'?styles.category+' '+styles.selected:styles.category}>Classics</span>
-                <span onClick={()=>{setCat('')}}
+                <span onClick={()=>{setCat('fiction')}}
                 className={cat=='fiction'?styles.category+' '+styles.selected:styles.category}>Fiction</span>
-                <span onClick={()=>{setCat('')}}
+                <span onClick={()=>{setCat('historical')}}
                 className={cat=='historical'?styles.category+' '+styles.selected:styles.category}>Historical</span>
-                <span onClick={()=>{setCat('')}}
+                <span onClick={()=>{setCat('science fiction')}}
                 className={cat=='science fiction'?styles.category+' '+styles.selected:styles.category}>Science Fiction</span>
-                <span onClick={()=>{setCat('')}}
+                <span onClick={()=>{setCat('fantasy')}}
                 className={cat=='fantasy'?styles.category+' '+styles.selected:styles.category}>Fantasy</span>
-                <span onClick={()=>{setCat('')}}
+                <span onClick={()=>{setCat('young adult')}}
                 className={cat=='young adult'?styles.category+' '+styles.selected:styles.category}>Young Adult</span>
+                <h3>Minimum rating</h3>
+                <h4>{minRat} / 5</h4>
+                <input value={minRat} onChange={(e)=>{setMinRat(parseFloat(e.target.value))}} type='range' min='0.0' max='5.0'/>
             </div>
             <div className={styles.all}>
                 {
-                    allJsonData.map((p)=>(
-                        <BookCard data={p}/>
-                    ))
+                    allJsonData.map((p)=>{
+                      if (p.genres) {
+                        if (p.genres.toLowerCase().includes(cat)) {
+                          if (p.rating >= minRat) {
+                            if (p.title.toLowerCase().includes(search)) {
+                              return <BookCard data={p}/>
+                            }
+                            
+                          }
+                        }
+                      }else{
+                        if (cat == '') {
+                          return <BookCard data={p}/>
+                        }
+                      }
+                      
+                      })
                 }
             </div>
         </div> 
